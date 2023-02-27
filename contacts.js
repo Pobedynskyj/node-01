aec8be90da06ade92ff14ccb7ac2068e648a0c24
Pobedynskyj Volodymyr <punkglue1@gmail.com>
2023-02-27 18:14:29 +0200

    create task

--- contacts.js
+++ contacts.js
@@ -1,23 +1,39 @@
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
+require("colors");
 
 const contactsPath = path.join("db", "contacts.json");
 console.log(contactsPath);
 
-function listContacts() {
-  fs.readFile(contactsPath);
+async function listContacts() {
+  const list = await fs.readFile(contactsPath);
+  return JSON.parse(list);
 }
 
-function getContactById(id) {
-  fs.readFile(contactsPath);
+async function getContactById(contactId) {
+  const data = await listContacts();
+  const contactById = data.find(({ id }) => id === contactId);
+  return contactById;
 }
 
-function removeContact(id) {
-  fs.readFile(contactsPath);
+async function removeContact(contactId) {
+  const data = await listContacts();
+  const deleteContacts = data.filter(({ id }) => id !== contactId);
+  await fs.writeFile(contactsPath, JSON.stringify(deleteContacts));
+  console.log(`Contact ${contactId} was removed!`.red);
 }
 
-function addContact(name, email, phone) {
-  fs.writeFile(contactsPath);
+async function addContact(name, email, phone) {
+  const newContact = {
+    id: new Date().toISOString(),
+    name,
+    email,
+    phone,
+  };
+  const data = await listContacts();
+  const newData = [...data, newContact];
+  await fs.writeFile(contactsPath, JSON.stringify(newData));
+  console.log(`Contact "${name}" was added!`.green);
 }
 
 module.exports = { listContacts, getContactById, removeContact, addContact };
